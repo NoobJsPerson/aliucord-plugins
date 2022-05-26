fae8557f04d35e9550db865089e0a4b1bfcee6e4
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2022-05-26 12:08:01 +0100

    finish NumbersToWords

--- NumbersToWords/src/main/java/com/aliucord/plugins/NumbersToWords.java
+++ NumbersToWords/src/main/java/com/aliucord/plugins/NumbersToWords.java
@@ -13,18 +13,51 @@ import com.discord.widgets.chat.input.ChatInputViewModel;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import kotlin.jvm.functions.Function1;
 
 @SuppressWarnings("unused")
 @AliucordPlugin
 public class NumbersToWords extends Plugin {
-    List<String> Numbers = Arrays.asList("zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen");
-    List<String> BiggerNumbers = Arrays.asList("twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred","thousand","million","billion");
+    List<String> Numbers = Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
+    List<String> BiggerNumbers = Arrays.asList("twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "thousand", "million", "billion");
+    private String specialBuild(int num){
+        int twodnum = num % 100;
+        if(twodnum == 0) return "";
+        String numstr = "";
+        if(twodnum <= 19) numstr = Numbers.get(twodnum);
+        else {
+            int bigNum = (int) Math.floor(twodnum / 10);
+            numstr = BiggerNumbers.get(bigNum - 2);
+            if (twodnum != bigNum * 10)
+                numstr += " "+Numbers.get(twodnum - bigNum * 10);
+        }
+        return numstr;
+    }
+    private String buildHundreds(int checkWith, String numstr){
+        String returnstr = numstr;
+        if(checkWith % 1000 >= 100) {
+            int test = (int) Math.floor((checkWith % 1000) / 100);
+            returnstr = Numbers.get(test) + " " + BiggerNumbers.get(8) + (!Objects.equals(numstr, "") ? " and "+numstr:"");
+        }
+        return returnstr;
+    }
+    private String complexBuild(int checkFor, int checkWith, int index, String numstr){
+        String returnstr = numstr;
+        int rest = checkFor != 1000000000 ? checkWith % (checkFor*1000) : checkWith;
+        if(rest >= checkFor) {
+            int smallerNum = (int) Math.floor(rest / checkFor);
+            returnstr = buildHundreds(smallerNum,specialBuild(smallerNum))+" "+ BiggerNumbers.get(index) + (!Objects.equals(numstr, "") ? " and "+numstr:"");
+        }
+        return returnstr;
+    }
     @Override
     public void start(Context context) throws NoSuchMethodException {
         patcher.patch(ChatInputViewModel.class.getDeclaredMethod("sendMessage", Context.class, MessageManager.class, MessageContent.class, List.class, boolean.class, Function1.class),
                 new PreHook(cf -> {
+
+
                     var content = (MessageContent) cf.args[2];
                     try {
                         var mes = content.component1().trim()+" ";
@@ -33,38 +66,15 @@ public class NumbersToWords extends Plugin {
                             try {
                                 int num = Integer.parseInt(string);
                                 if(num == 0){
-                                    newmes += "0 ";
+                                    newmes += "zero ";
                                     continue;
                                 }
                                 String numstr = "";
-                                int twodnum = num % 100;
-                                if(twodnum == 0) continue;
-                                if(twodnum <= 19) numstr = Numbers.get(num);
-                                else {
-                                    int bigNum = (int) Math.floor(twodnum / 10);
-                                    numstr = BiggerNumbers.get(bigNum - 2)+" ";
-                                    if (twodnum != bigNum * 10)
-                                        numstr += Numbers.get(twodnum - bigNum * 10);
-                                }
-                                int threednum = num % 1000;
-                                if(threednum == 0) continue;
-                                int bigNum = (int) Math.floor(threednum / 100);
-                                numstr = Numbers.get(bigNum)+" "+BiggerNumbers.get(9)+" and "+numstr;
-
-                                int fourdnum = num % 10000;
-                                if(fourdnum == 0) continue;
-                                int bigNum2 = (int) Math.floor(fourdnum / 1000);
-                                numstr = Numbers.get(bigNum2)+" "+BiggerNumbers.get(10)+" and "+numstr;
-
-                                int sixdnum = num % 10000000;
-                                if(sixdnum == 0) continue;
-                                int bigNum3 = (int) Math.floor(sixdnum / 1000000);
-                                numstr = Numbers.get(bigNum)+" "+BiggerNumbers.get(11)+" and "+numstr;
-
-                                if(num >= 1000000000) continue;
-                                int bigNum4 = (int) Math.floor(num / 1000000000);
-                                numstr = Numbers.get(bigNum)+" "+BiggerNumbers.get(12)+" and "+numstr;
-
+                                numstr = specialBuild(num);
+                                numstr = buildHundreds(num,numstr);
+                                numstr = complexBuild(1000,num,9,numstr);
+                                numstr = complexBuild(1000000,num,10,numstr);
+                                numstr = complexBuild(1000000000,num,11,numstr);
                                 newmes += numstr+" ";
                                 continue;
                             } catch(NumberFormatException nfe){
